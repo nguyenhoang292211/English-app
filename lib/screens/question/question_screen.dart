80e520ca57c71605fba069c66450934baadfce4d
yenlee38 <ngocyen174308@gmail.com>
2022-05-05 22:59:14 +0700

    change

--- lib/screens/question/question_screen.dart
+++ lib/screens/question/question_screen.dart
@@ -5,40 +5,41 @@ import 'package:vocabulary_learning/controllers/question_grammar_controller.dart
 import 'package:vocabulary_learning/screens/question/components/HeaderQuestion.dart';
 import 'package:vocabulary_learning/screens/question/components/process_bar.dart';
 import 'package:vocabulary_learning/screens/question/components/question_card.dart';
-class QuestionScreen extends StatelessWidget {
 
-    const QuestionScreen({ Key? key }) : super(key: key);
-     final String skip = "Skip";
-    @override
-    Widget build(BuildContext context) {
-      final size = MediaQuery.of(context).size;
-      QuestionGrammarController questionGrammarController = Get.put(QuestionGrammarController());
-      return Scaffold(
+class QuestionScreen extends StatelessWidget {
+  const QuestionScreen({Key? key}) : super(key: key);
+  final String skip = "Skip";
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    QuestionGrammarController questionGrammarController =
+        Get.put(QuestionGrammarController());
+    return Scaffold(
         backgroundColor: kNudeQuestion,
         body: Padding(
           padding: const EdgeInsets.all(30.0),
           child: Column(
             children: [
-            
-            HeaderQuestion(),
-            ProcessBar(),
-          
-            Obx(
+              HeaderQuestion(),
+              ProcessBar(),
+              Obx(
                 () => Expanded(
-                child: PageView.builder(
-                  // Block swipe to next qn
-                  physics: NeverScrollableScrollPhysics(),
-                  controller: questionGrammarController.pageController,
-                 itemCount: questionGrammarController.lQuestion.length,
-                 onPageChanged: questionGrammarController.updateNumberQuestion,
-                  itemBuilder: (context, index) => QuestionCard(
-                    questionGrammar: questionGrammarController.lQuestion[index],
-                     ),
+                  child: PageView.builder(
+                    // Block swipe to next qn
+                    physics: NeverScrollableScrollPhysics(),
+                    controller: questionGrammarController.pageController,
+                    itemCount: questionGrammarController.lQuestion.length,
+                    onPageChanged:
+                        questionGrammarController.updateNumberQuestion,
+                    itemBuilder: (context, index) => QuestionCard(
+                      questionGrammar:
+                          questionGrammarController.lQuestion[index],
+                    ),
+                  ),
                 ),
-              ),)
+              )
             ],
           ),
-        )
-      );
-    }
+        ));
   }
+}
